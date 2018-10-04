20e485475d38f17a4327442b766e134fce00812b
ChristopheRibault <c.ribault.dev@gmail.com>
2018-10-04 20:55:59 +0200

    mise en page cards

--- src/Results.js
+++ src/Results.js
@@ -9,14 +9,15 @@ class Results extends Component{
     return (
       <div className='Results'>
         { this.props.results && results.map(movie =>
+          movie.poster_path && 
           <Card
-            title = {movie.title}
-            vote_average= {movie.vote_average}
-            vote_count= {movie.vote_count}
-            overview= {movie.overview}
-            image= {`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-            id= {movie.id}
-          />
+          title = {movie.title}
+          vote_average= {movie.vote_average}
+          vote_count= {movie.vote_count}
+          overview= {movie.overview}
+          image= {`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+          id= {movie.id}
+        />
         )
         }
       </div>

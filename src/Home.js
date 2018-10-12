98c00831e67b938f813d75059b358727198c2bd5
ChristopheRibault <c.ribault.dev@gmail.com>
2018-10-12 10:56:05 +0200

    ajout section released this week

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Popular from './Popular';
+import LastReleased from './LastReleased';
 import './Home.css';
 
 
@@ -7,7 +8,8 @@ const API_KEY='17e0f34221767f1716a0e3a321214fb3';
 
 class Home extends Component {
   state = {
-    popular: []
+    popular: [],
+    latest:[]
   }
 
   findPop = async () =>{
@@ -17,11 +19,22 @@ class Home extends Component {
     this.setState({
       popular: data.results.filter((_,i)=>i<10)
     })
-    console.log(this.state.data)
+  }
+
+  findLatest = async () =>{
+    const ends = `${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDate()}`;
+    const begins = `${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDate()-7}`
+    const url = `https://api.themoviedb.org/3/discover/movie?&api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&primary_release_date.gte=${begins}&primary_release_date.lte=${ends}`;
+    const call_api = await fetch(url);
+    const data = await call_api.json()
+    this.setState({
+      latest: data.results.filter((_,i)=>i<10)
+    })
   }
 
   componentDidMount(){
     this.findPop();
+    this.findLatest();
   }
 
   render(){
@@ -29,6 +42,9 @@ class Home extends Component {
       <div className='Home'>
         <Popular
           data={this.state.popular}
+        />
+        <LastReleased 
+          data={this.state.latest}
         />
       </div>
     );
--- src/LastReleased.js
+++ src/LastReleased.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import './Home.css';
 
 export default class Popular extends Component{
+
   render(){
     return(
-      <section className='Popular'>
-          <h2>TOP 10 popular movies</h2>
+      <section className='LastReleased'>
+          <h2>Released this week</h2>
           <div className='list'>
             {this.props.data.map((movie,i)=>
-              <div key={`popular-${i}`} className='item'>
+              <div key={`latest-${i}`} className='item'>
                 <img src= {`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title}/>
                 <h3>{i+1}- {movie.title}</h3>
+                <small>Realeased on {movie.release_date}</small>
               </div>
             )}
           </div>

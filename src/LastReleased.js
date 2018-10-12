d6d05a4ed8ef46db37d8637825f1ecfac22d0bca
ChristopheRibault <c.ribault.dev@gmail.com>
2018-10-12 13:53:48 +0200

    chnagement style

--- src/LastReleased.js
+++ src/LastReleased.js
@@ -6,17 +6,17 @@ export default class Popular extends Component{
   render(){
     return(
       <section className='LastReleased'>
-          <h2>Released this week</h2>
-          <div className='list'>
-            {this.props.data.map((movie,i)=>
-              <div key={`latest-${i}`} className='item'>
-                <img src= {`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title}/>
-                <h3>{i+1}- {movie.title}</h3>
-                <small>Realeased on {movie.release_date}</small>
-              </div>
-            )}
-          </div>
-        </section>
+        <h2>Released this week</h2>
+        <div className='list'>
+          {this.props.data.map((movie,i)=>
+            <div key={`latest-${i}`} className='item'>
+              <img src= {`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title}/>
+              <h3>{i+1}- {movie.title}</h3>
+              <small>Realeased on {movie.release_date}</small>
+            </div>
+          )}
+        </div>
+      </section>
     );
   }
 }
--- src/Popular.js
+++ src/Popular.js
@@ -5,16 +5,16 @@ export default class Popular extends Component{
   render(){
     return(
       <section className='Popular'>
-          <h2>TOP 10 popular movies</h2>
-          <div className='list'>
-            {this.props.data.map((movie,i)=>
-              <div key={`popular-${i}`} className='item'>
-                <img src= {`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title}/>
-                <h3>{i+1}- {movie.title}</h3>
-              </div>
-            )}
-          </div>
-        </section>
+        <h2>TOP 10 popular movies</h2>
+        <div className='list'>
+          {this.props.data.map((movie,i)=>
+            <div key={`popular-${i}`} className='item'>
+              <img src= {`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title}/>
+              <h3>{i+1}- {movie.title}</h3>
+            </div>
+          )}
+        </div>
+      </section>
     );
   }
 }
